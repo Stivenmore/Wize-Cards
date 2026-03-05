bf26bb7efcc33d0ee175b123e38827a2d25f5e75
Benny <benny.reyes@wizeline.com>
2026-03-05 15:41:07 -0600

    Updating to TextButton

--- lib/features/login/presentation/widgets/terms_and_conditions_widget.dart
+++ lib/features/login/presentation/widgets/terms_and_conditions_widget.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:wize_cards/core/utils/constant.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:wize_cards/core/utils/constant.dart';
 import 'package:wize_cards/features/login/presentation/constants/login_screen_constants.dart';
 
 class TermsAndConditionsWidget extends StatelessWidget {
@@ -9,39 +8,38 @@ class TermsAndConditionsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 320,
-      child: RichText(
-        textAlign: TextAlign.center,
-        maxLines: 2,
-        overflow: TextOverflow.ellipsis,
-        text: TextSpan(
-          style: const TextStyle(
-            color: ColorConstants.hint, 
+    return Wrap(
+      alignment: WrapAlignment.center,
+      crossAxisAlignment: WrapCrossAlignment.center,
+      children: [
+        Text(
+          LoginScreenConstants.termsMessage,
+          style: TextStyle(
+            color: ColorConstants.hint,
             fontSize: TextSizeConstants.caption,
           ),
-          children: [
-            TextSpan(text: LoginScreenConstants.termsMessage),
-            TextSpan(
-              text: LoginScreenConstants.termsLink,
-              style: const TextStyle(
-                color: ColorConstants.hint,
-                decoration: TextDecoration.underline,
-                fontWeight: FontWeight.bold
-              ),
-              recognizer: TapGestureRecognizer()
-              ..onTap = () async {
-                  final Uri url = Uri.parse(AppConstants.apiBaseUrl);
-                  if (await canLaunchUrl(url)) {
-                    await launchUrl(url);
-                  } else {
-                    throw "Can't open url: $url";
-                  }
-              },
+        ),
+        TextButton(
+          style: TextButton.styleFrom(
+            padding: EdgeInsets.zero,
+            minimumSize: Size.zero,
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          ),
+          onPressed: () {
+            final Uri url = Uri.parse(AppConstants.apiBaseUrl);
+            launchUrl(url);
+          },
+          child: Text(
+            LoginScreenConstants.termsLink,
+            style: TextStyle(
+              color: ColorConstants.hint,
+              fontSize: TextSizeConstants.caption,
+              decoration: TextDecoration.underline,
+              fontWeight: FontWeight.bold,
             ),
-          ],
-        ), 
-      ),
+          ),
+        ),
+      ],
     );
   }
 }
