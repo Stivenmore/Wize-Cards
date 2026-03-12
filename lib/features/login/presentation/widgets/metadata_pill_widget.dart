c7438ec12889c6f78afb9cc5341255a218f5af59
isaacsanchez-wizeline <isaac.sanchez@wizeline.com>
2026-03-11 18:00:02 -0700

    Feat: Create Metadata Pill Widget

---
+++ lib/features/login/presentation/widgets/metadata_pill_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:wize_cards/core/utils/constant.dart';
+
+class MetadataPillWidget extends StatelessWidget {
+  final String text;
+
+  const MetadataPillWidget({
+    super.key,
+    required this.text
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 72,
+      height: 28,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Theme.of(context).hoverColor,
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Theme.of(context).hintColor,
+          fontSize: TextSizeConstants.caption
+          ),
+      ),
+    );
+  }
+}
