0389534bd86aa10ed81bf6b7df28571bc0a4d725
isaacsanchez-wizeline <isaac.sanchez@wizeline.com>
2026-03-13 11:56:06 -0700

    Feat: Rework of entire widget - Use constants, declare textstyle object, replace container with sizedbox

--- lib/features/login/presentation/widgets/title_sesion_state_wdiget.dart
+++ lib/features/login/presentation/widgets/title_sesion_state_wdiget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wize_cards/core/utils/color_constants.dart';
 import 'package:wize_cards/core/utils/constant.dart';
 
 class TitleSesionStateWdiget extends StatelessWidget {
@@ -8,44 +9,40 @@ class TitleSesionStateWdiget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(SpacingConstants.xs),
-      child: Column(
-        mainAxisAlignment: .center,
-        children: [
-          Text(
-            title.toUpperCase(),
-            style: TextStyle(
-              color: Theme.of(context).hintColor,
-              fontSize: TextSizeConstants.caption,
-              fontWeight: .bold,
-              letterSpacing: 1.0,
-            ),
+    TextStyle textStyle = const TextStyle(
+      fontSize: TextSizeConstants.caption,
+      fontWeight: FontWeight.bold,
+      color: ColorConstants.textPrimary,
+    );
+
+    return Column(
+      mainAxisAlignment: .center,
+      children: [
+        Text(
+          title.toUpperCase(),
+          style: textStyle.copyWith(
+            color: Theme.of(context).hintColor,
+            letterSpacing: 1.0,
           ),
-          Row(
-            mainAxisAlignment: .center,
-            spacing: SpacingConstants.small,
-            children: [
-              Container(
-                width: 8,
-                height: 8,
+        ),
+        Row(
+          mainAxisAlignment: .center,
+          spacing: SpacingConstants.small,
+          children: [
+            SizedBox(
+              width: SizeConstants.dots,
+              height: SizeConstants.dots,
+              child: DecoratedBox(
                 decoration: BoxDecoration(
-                  color: Colors.green,
+                  color: ColorConstants.dotGreen,
                   shape: BoxShape.circle,
                 ),
               ),
-              Text(
-                "Live Session",
-                style: TextStyle(
-                  fontSize: TextSizeConstants.caption,
-                  fontWeight: .bold,
-                  color: Colors.black,
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
+            ),
+            Text(TextConstans.liveSession, style: textStyle),
+          ],
+        ),
+      ],
     );
   }
 }
